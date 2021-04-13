19ad314c399ec2f90d9416241b838901c5673d0d
Karolina Hamerszmidt <karolina@hamerszmidt.eu>
2021-04-13 16:45:34 +0200

    Beauty

--- src/Beauty/Beauty.js
+++ src/Beauty/Beauty.js
@@ -1,6 +1,9 @@
 import React from "react";
 import "./Beauty.css";
 import beauty from "./beauty.jpg";
+import roza from "./roza.jpg";
+import blawatek from "./blawatek.jpg";
+import tulipan from "./tulipan.jpg";
 
 export const Beauty = () => (
   <div>
@@ -8,6 +11,17 @@ export const Beauty = () => (
     <div className="Beauty__banerBeauty">Beauty</div>
     <div className="Beauty__baner">
       <img className="Beauty__banerImage" src={beauty} />
+    </div>
+    <div className="Beauty__post">
+      <div className="Beauty__posty">
+        <img src={roza} />
+      </div>
+      <div className="Beauty__posty">
+        <img src={blawatek} />
+      </div>
+      <div className="Beauty__posty">
+        <img src={tulipan} />
+      </div>
     </div>
   </div>
 );
