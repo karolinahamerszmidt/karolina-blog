a723caccfe05c5911d388a3da5c5f63c7611ec2b
Karolina Hamerszmidt <karolina@hamerszmidt.eu>
2021-04-14 21:14:52 +0200

    Contact

--- src/Contact/Contact.js
+++ src/Contact/Contact.js
@@ -8,5 +8,19 @@ export const Contact = () => (
       If you have any questions or want to get in contact, please use the form
       below.
     </div>
+
+    <form className="Contact__form">
+      <input className="Contact__formFirstName" placeholder="First name" />
+      <input className="Contact__formLastName" placeholder="Last name" />
+      <input className="Contact__formEmail" placeholder="Email" />
+      <textarea
+        className="Contact__formMessage"
+        placeholder="Message"
+        rows={10}
+      />
+      <div className="Contact__formButton">
+        <button className="Contact__formButtonButton">Send</button>
+      </div>
+    </form>
   </div>
 );
