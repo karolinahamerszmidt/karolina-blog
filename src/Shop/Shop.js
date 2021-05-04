23d99346af01ba727c3b111035e28d93943e7951
Karolina Hamerszmidt <karolina@hamerszmidt.eu>
2021-05-04 12:01:52 +0200

    Shop

--- src/Shop/Shop.js
+++ src/Shop/Shop.js
@@ -1,4 +1,29 @@
 import React from "react";
 import "./Shop.css";
+import ring from "../ring.jpg";
+import ring_model from "../ring_model.jpg";
+import necklace from "../necklace.jpg";
+import necklace_model from "../necklace_model.jpg";
+import earrings from "../earrings.jpg";
+import earrings_model from "../earrings_model.jpg";
 
-export const Shop = () => <div className="Shop__product">Products</div>;
+export const Shop = () => (
+  <div className="Shop">
+    <div className="Shop__product">Products</div>
+
+    <div className="Shop__post">
+      <div className="Shop__posty">
+        <img src={ring} />
+        <img src={ring_model} />
+      </div>
+      <div className="Shop__posty">
+        <img src={necklace} />
+        <img src={necklace_model} />
+      </div>
+      <div className="Shop__posty">
+        <img src={earrings} />
+        <img src={earrings_model} />
+      </div>
+    </div>
+  </div>
+);
