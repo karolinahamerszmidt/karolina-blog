31e4c92aed14a98b60d414da4c4dd4d1ba75a666
Karolina Hamerszmidt <karolina@hamerszmidt.eu>
2021-05-07 10:55:36 +0200

    Shop

--- src/Shop/Shop.js
+++ src/Shop/Shop.js
@@ -9,20 +9,26 @@ import earrings_model from "../earrings_model.jpg";
 
 export const Shop = () => (
   <div className="Shop">
-    <div className="Shop__product">Products</div>
+    <div className="Shop__title">Products</div>
 
-    <div className="Shop__post">
-      <div className="Shop__posty">
-        <img src={ring} />
-        <img src={ring_model} />
+    <div className="Shop__products">
+      <div className="Shop__product">
+        <div className="Shop__productImage">
+          <img src={ring} />
+          <img src={ring_model} />
+        </div>
       </div>
-      <div className="Shop__posty">
-        <img src={necklace} />
-        <img src={necklace_model} />
+      <div className="Shop__product">
+        <div className="Shop__productImage">
+          <img src={necklace} />
+          <img src={necklace_model} />
+        </div>
       </div>
-      <div className="Shop__posty">
-        <img src={earrings} />
-        <img src={earrings_model} />
+      <div className="Shop__product">
+        <div className="Shop__productImage">
+          <img src={earrings} />
+          <img src={earrings_model} />
+        </div>
       </div>
     </div>
   </div>
