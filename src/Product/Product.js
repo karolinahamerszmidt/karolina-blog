e68f5efc64ed11f5c0b8a86a713dd44aa90a8e51
Karolina Hamerszmidt <karolina@hamerszmidt.eu>
2021-05-10 12:43:08 +0200

    Wish List

--- src/Product/Product.js
+++ src/Product/Product.js
@@ -8,6 +8,9 @@ import earrings_five from "../earrings_five.jpg";
 import earrings_six from "../earrings_six.jpg";
 import earrings_seven from "../earrings_seven.jpg";
 import { Carousel } from "react-responsive-carousel";
+import heart from "../heart.png";
+import add from "../add.png";
+import { faBlackberry } from "@fortawesome/free-brands-svg-icons";
 
 export const Product = () => {
   return (
@@ -42,6 +45,15 @@ export const Product = () => {
         <div className="Product__title">Silver earring Mara</div>
         <div className="Product__price">20 euro</div>
         <button className="Product__button">Add to cart</button>
+        <div className="Product__wishList">
+          <img className="Product__wishListHeart" src={heart} />
+          <div className="Product__wishListText">Wish list</div>
+        </div>
+        <div className="Product__description">
+          <img className="Product__descriptionAdd" src={add} />
+          <div className="Product__descriptionText">Description</div>
+        </div>
+        <div className="Product__line" />
       </div>
     </div>
   );
